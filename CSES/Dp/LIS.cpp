e84361c27db2665d9589cd4868fc872a02740769
Yadnyesh Patil <yadnyeshpatil60@gmail.com>
2025-04-03 12:46:27 +0530

    CSES Added

--- CSES/Dp/LIS.cpp
+++ CSES/Dp/LIS.cpp
@@ -1,23 +1,36 @@
 // Problem: Longest Increasing Subsequence
-// 
+// CSES Link - https://cses.fi/problemset/task/1145
 
-class Solution {
-  public:
-  
-      int solve(int ind,int prev_ind, vector<int>& nums, vector<vector<int>>& dp) {
-          if(ind==nums.size()) return 0;
-          if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-  
-          int len=0+solve(ind+1, prev_ind, nums, dp);
-          if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
-              len=max(len, 1+solve(ind+1, ind, nums, dp));
-          }
-          return dp[ind][prev_ind+1]=len;
-      }
-      
-      int lengthOfLIS(vector<int>& nums) {
-          int n=nums.size();
-          vector<vector<int>> dp(n, vector<int>(n+1, -1));
-          return solve(0, -1, nums, dp);
-      }
-  };
+#include <bits/stdc++.h>
+//#define all(s) s.begin(), s.end()
+using namespace std;
+using ll=long long;
+using ull=unsigned long long;
+
+const int _N=1e5+5;
+
+int solve(int ind,int prev_ind, vector<int>& nums, vector<vector<int>>& dp) {
+        if(ind==nums.size()) return 0;
+        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
+
+        int len=0+solve(ind+1, prev_ind, nums, dp);
+        if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
+            len=max(len, 1+solve(ind+1, ind, nums, dp));
+        }
+        return dp[ind][prev_ind+1]=len;
+    }
+
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int n;
+	cin>>n;
+	vector<int> nums(n);
+	for(int i=0;i<n;i++) cin>>nums[i];
+	
+	vector<vector<int>> dp(n, vector<int>(n+1, -1));
+	
+	cout<<solve(0, -1, nums, dp);
+	
+	return 0;
+}
